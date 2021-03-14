482923c1537676414b118b1c70be02d07b69af80
Adriana-Giol <gioladriana5@gmail.com>
2021-03-14 16:49:55 +0200

    Clasa derivata Sarpe
    Atribute, Constructori, ToString

---
+++ 1. Laborator/2. Seminar 2/[Com]Ciurea_Seminar2_1046/Sarpe.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Com_Ciurea_Seminar2_1046
+{
+    class Sarpe:Animal
+    {
+        //Atribute
+        private int lungime;
+        private bool esteVeninos;
+
+
+        //Constructor implicit
+        public Sarpe() : base()
+        {
+            this.lungime = 0;
+            this.esteVeninos = false;
+        }
+
+        //Constructor implicit
+        public Sarpe(int varsta, string nume, float greutate, int lungime, bool esteVeninos) : base(varsta,nume, greutate)
+        {
+            this.lungime = lungime;
+            this.esteVeninos = esteVeninos;
+        }
+
+        //ToString
+        public override string ToString()
+        {
+            return base.ToString() + " lungimea " + lungime + "si este veninos " + esteVeninos;
+        }
+    }
+}
