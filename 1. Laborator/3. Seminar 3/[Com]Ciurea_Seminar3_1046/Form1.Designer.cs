e8b4f3c7736ea0609c568139af34e0a3533cf573
Adriana-Giol <gioladriana5@gmail.com>
2021-03-16 20:27:43 +0200

    Update Form1.Designer.cs

--- 1. Laborator/3. Seminar 3/[Com]Ciurea_Seminar3_1046/Form1.Designer.cs
+++ 1. Laborator/3. Seminar 3/[Com]Ciurea_Seminar3_1046/Form1.Designer.cs
@@ -28,20 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelCod = new System.Windows.Forms.Label();
+            this.labelSex = new System.Windows.Forms.Label();
+            this.labelVarsta = new System.Windows.Forms.Label();
+            this.labelNume = new System.Windows.Forms.Label();
+            this.tbCod = new System.Windows.Forms.TextBox();
+            this.tbVarsta = new System.Windows.Forms.TextBox();
+            this.tbNume = new System.Windows.Forms.TextBox();
+            this.cbSex = new System.Windows.Forms.ComboBox();
+            this.tbNote = new System.Windows.Forms.TextBox();
+            this.labelNote = new System.Windows.Forms.Label();
+            this.btnCreareStudent = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // labelCod
+            // 
+            this.labelCod.AutoSize = true;
+            this.labelCod.Location = new System.Drawing.Point(49, 28);
+            this.labelCod.Name = "labelCod";
+            this.labelCod.Size = new System.Drawing.Size(37, 17);
+            this.labelCod.TabIndex = 0;
+            this.labelCod.Text = "Cod:";
+            // 
+            // labelSex
+            // 
+            this.labelSex.AutoSize = true;
+            this.labelSex.Location = new System.Drawing.Point(49, 86);
+            this.labelSex.Name = "labelSex";
+            this.labelSex.Size = new System.Drawing.Size(35, 17);
+            this.labelSex.TabIndex = 1;
+            this.labelSex.Text = "Sex:";
+            // 
+            // labelVarsta
+            // 
+            this.labelVarsta.AutoSize = true;
+            this.labelVarsta.Location = new System.Drawing.Point(49, 151);
+            this.labelVarsta.Name = "labelVarsta";
+            this.labelVarsta.Size = new System.Drawing.Size(53, 17);
+            this.labelVarsta.TabIndex = 2;
+            this.labelVarsta.Text = "Varsta:";
+            // 
+            // labelNume
+            // 
+            this.labelNume.AutoSize = true;
+            this.labelNume.Location = new System.Drawing.Point(49, 221);
+            this.labelNume.Name = "labelNume";
+            this.labelNume.Size = new System.Drawing.Size(49, 17);
+            this.labelNume.TabIndex = 3;
+            this.labelNume.Text = "Nume:";
+            // 
+            // tbCod
+            // 
+            this.tbCod.Location = new System.Drawing.Point(157, 28);
+            this.tbCod.Name = "tbCod";
+            this.tbCod.Size = new System.Drawing.Size(121, 22);
+            this.tbCod.TabIndex = 4;
+            // 
+            // tbVarsta
+            // 
+            this.tbVarsta.Location = new System.Drawing.Point(157, 151);
+            this.tbVarsta.Name = "tbVarsta";
+            this.tbVarsta.Size = new System.Drawing.Size(121, 22);
+            this.tbVarsta.TabIndex = 5;
+            // 
+            // tbNume
+            // 
+            this.tbNume.Location = new System.Drawing.Point(157, 221);
+            this.tbNume.Name = "tbNume";
+            this.tbNume.Size = new System.Drawing.Size(121, 22);
+            this.tbNume.TabIndex = 6;
+            // 
+            // cbSex
+            // 
+            this.cbSex.FormattingEnabled = true;
+            this.cbSex.Items.AddRange(new object[] {
+            "M",
+            "F"});
+            this.cbSex.Location = new System.Drawing.Point(157, 86);
+            this.cbSex.Name = "cbSex";
+            this.cbSex.Size = new System.Drawing.Size(121, 24);
+            this.cbSex.TabIndex = 7;
+            // 
+            // tbNote
+            // 
+            this.tbNote.Location = new System.Drawing.Point(157, 279);
+            this.tbNote.Name = "tbNote";
+            this.tbNote.Size = new System.Drawing.Size(121, 22);
+            this.tbNote.TabIndex = 8;
+            // 
+            // labelNote
+            // 
+            this.labelNote.AutoSize = true;
+            this.labelNote.Location = new System.Drawing.Point(49, 279);
+            this.labelNote.Name = "labelNote";
+            this.labelNote.Size = new System.Drawing.Size(42, 17);
+            this.labelNote.TabIndex = 9;
+            this.labelNote.Text = "Note:";
+            // 
+            // btnCreareStudent
+            // 
+            this.btnCreareStudent.Location = new System.Drawing.Point(91, 347);
+            this.btnCreareStudent.Name = "btnCreareStudent";
+            this.btnCreareStudent.Size = new System.Drawing.Size(116, 33);
+            this.btnCreareStudent.TabIndex = 10;
+            this.btnCreareStudent.Text = "Creare Student";
+            this.btnCreareStudent.UseVisualStyleBackColor = true;
+            this.btnCreareStudent.Click += new System.EventHandler(this.btnCreareStudent_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCreareStudent);
+            this.Controls.Add(this.labelNote);
+            this.Controls.Add(this.tbNote);
+            this.Controls.Add(this.cbSex);
+            this.Controls.Add(this.tbNume);
+            this.Controls.Add(this.tbVarsta);
+            this.Controls.Add(this.tbCod);
+            this.Controls.Add(this.labelNume);
+            this.Controls.Add(this.labelVarsta);
+            this.Controls.Add(this.labelSex);
+            this.Controls.Add(this.labelCod);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelCod;
+        private System.Windows.Forms.Label labelSex;
+        private System.Windows.Forms.Label labelVarsta;
+        private System.Windows.Forms.Label labelNume;
+        private System.Windows.Forms.TextBox tbCod;
+        private System.Windows.Forms.TextBox tbVarsta;
+        private System.Windows.Forms.TextBox tbNume;
+        private System.Windows.Forms.ComboBox cbSex;
+        private System.Windows.Forms.TextBox tbNote;
+        private System.Windows.Forms.Label labelNote;
+        private System.Windows.Forms.Button btnCreareStudent;
     }
 }
