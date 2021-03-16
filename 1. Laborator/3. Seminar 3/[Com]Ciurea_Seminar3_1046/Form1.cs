e6a27e6440e7d59cec8c60033ebd5dbaab5882e2
Adriana-Giol <gioladriana5@gmail.com>
2021-03-16 20:27:38 +0200

    Update Form1.cs

--- 1. Laborator/3. Seminar 3/[Com]Ciurea_Seminar3_1046/Form1.cs
+++ 1. Laborator/3. Seminar 3/[Com]Ciurea_Seminar3_1046/Form1.cs
@@ -17,7 +17,7 @@ namespace _Com_Ciurea_Seminar3_1046
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void btnCreareStudent_Click(object sender, EventArgs e)
         {
 
         }
