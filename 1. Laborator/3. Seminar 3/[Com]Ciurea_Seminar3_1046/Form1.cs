b8b07413e5372894b0a04270c3de8ae970d4c740
Adriana-Giol <gioladriana5@gmail.com>
2021-03-17 07:50:21 +0200

    Update Form1.cs

--- 1. Laborator/3. Seminar 3/[Com]Ciurea_Seminar3_1046/Form1.cs
+++ 1. Laborator/3. Seminar 3/[Com]Ciurea_Seminar3_1046/Form1.cs
@@ -17,9 +17,21 @@ namespace _Com_Ciurea_Seminar3_1046
             InitializeComponent();
         }
 
+        /*Avem 2  metode cu aceeasi semnatura si cand vreau sa folosesc una dintre ele ma duc in Form1.Designer.cs
+            si la implementarea butonului - unde am implementat evenimentul de click doar schimb referinta 
+            this.metoda1 sau this.metoda2 sau pot sa dublez evenimentul de click pentru a le apela pe mandoua. 
+        Pot sa mai apelez o metoda cu += apoi sa o scot cu -=  (metoda statica); Varianta dinamica e sa scriu in metoda care vreau in continuare 
+        sa apara, dezabonarea metodei care nu vreau sa mai apara cu ajutorul operatorului -= */ 
+
         private void btnCreareStudent_Click(object sender, EventArgs e)
         {
+            Console.WriteLine("Am facut click pe buton");
+        }
 
+        private void metodaMea(object sender, EventArgs e)
+        {
+            btnCreareStudent.Click -= new EventHandler(btnCreareStudent_Click);
+            Console.WriteLine("Am facut click prin metoda mea!");
         }
     }
 }
