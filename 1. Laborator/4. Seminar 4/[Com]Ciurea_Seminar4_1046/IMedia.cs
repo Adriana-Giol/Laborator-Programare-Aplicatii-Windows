48da7a2fe27553b516902b52ecc7bcc3c7013ef4
Adriana-Giol <gioladriana5@gmail.com>
2021-03-19 15:09:07 +0200

    Create IMedia.cs

---
+++ 1. Laborator/4. Seminar 4/[Com]Ciurea_Seminar4_1046/IMedia.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Com_Ciurea_Seminar4_1046
+{
+    interface IMedia
+    {
+        float calculeazaMedie();
+    }
+}
