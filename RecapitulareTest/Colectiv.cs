955c3d0263dc5cab2287f2fd9dfe6b5a338a65b5
Adriana-Giol <gioladriana5@gmail.com>
2021-04-21 23:24:12 +0300

    UPDATE 11:24

--- RecapitulareTest/Colectiv.cs
+++ RecapitulareTest/Colectiv.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RecapitulareTest
 {
-    class Colectiv
+    class Colectiv:ICloneable
     {
         //1. DEFINIRE ATRIBUTE
         private String numeColectiv;
@@ -32,6 +32,47 @@ namespace RecapitulareTest
             }
         }
 
-        //4. 
+        //4. PROPRIETATI
+        public string NumeColectiv { get => numeColectiv; set => numeColectiv = value; }
+        public List<Persoana> ListaPersoane { get => listaPersoane; set => listaPersoane = value; }
+
+      
+
+
+        //5. TO STRING
+        public override string ToString()
+        {
+            string rezultat = "In colectivul " + numeColectiv + " sunt inregistrare urmatoarele persoane " + Environment.NewLine;
+            foreach (Persoana p in listaPersoane)
+                rezultat += p.ToString() + Environment.NewLine;
+            return rezultat;
+        }
+
+        //6. ICLONEABLE
+
+        public object Clone()
+        {
+            Colectiv clona = (Colectiv)this.MemberwiseClone();
+            List<Persoana> listaNoua = new List<Persoana>();
+            foreach (Persoana p in listaPersoane)
+                listaNoua.Add((Persoana)p.Clone());
+            clona.listaPersoane = listaNoua;
+            return clona;
+        }
+
+
+        //7. OPERATOR INDEX
+        public Persoana this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < listaPersoane.Count && listaPersoane != null)
+                    return listaPersoane[index];
+                else
+                    return null;
+            }
+        }
+
+        //8. OPERATOR+
     }
 }
