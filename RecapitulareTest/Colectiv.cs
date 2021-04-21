8786ed33a0403616f5eda203920893a907da092d
Adriana-Giol <gioladriana5@gmail.com>
2021-04-21 20:53:29 +0300

    Update 8:53

---
+++ RecapitulareTest/Colectiv.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecapitulareTest
+{
+    class Colectiv
+    {
+        //1. DEFINIRE ATRIBUTE
+        private String numeColectiv;
+        private List<Persoana> listaPersoane;
+
+
+        //2. CONSTRUCTOR FARA PARAMETRI
+        public Colectiv()
+        {
+            this.numeColectiv = "Bucuria";
+            this.listaPersoane = new List<Persoana>();
+        }
+
+
+        //3. CONSTRUCTOR CU PARAMETRI 
+        public Colectiv(string numeColectiv, List<Persoana> listaPersoane)
+        {
+            this.numeColectiv = numeColectiv;
+            this.listaPersoane = new List<Persoana>();
+            foreach(Persoana c in listaPersoane)
+            {
+                this.listaPersoane.Add(c);
+            }
+        }
+
+        //4. 
+    }
+}
