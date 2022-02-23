41ec9b33bd13e84aa8f1c0d366aab2ba800fb044
Adriana-Giol <gioladriana5@gmail.com>
2022-02-23 03:22:01 +0200

    Update Movie Class

--- 1. Code/S02_IerarhieDeClase/Movie.cs
+++ 1. Code/S02_IerarhieDeClase/Movie.cs
@@ -9,61 +9,71 @@ namespace S02_IerarhieDeClase
     class Movie
     {
         //Definire atribute
-        string title;                    //definire implicita private
-        private DateTime releaseDate;   //definire explicita private
-
-        public float profit;
-        protected string genre;
+        private string title;
+        private DateTime releaseDate;
+        private float profit;
+        private string genre;
         private string platform;
 
-        //Constructor implicit - Varianta 1
+
+        /*      //Constructor implicit - Varianta 1
+              public Movie()
+              {
+
+              }
+        */
+
+        //Constructor implicit - Varianta 2
         public Movie()
         {
-
+            this.title = " ";
+            this.releaseDate = System.DateTime.Now;
+            this.profit = 0.0f;
+            this.genre = "";
+            this.platform = " ";
         }
-        //Constructor implicit - Varianta 2
-/*      public Movie()
-      {
-          this.title = " ";
-          this.releaseDate = System.DateTime.Now;
-          this.profit = 0.0f;
-          this.genre = "";
-          this.platform = " ";
-      }
-*/
 
 
-      //Constructor cu parametri
-      public Movie(string title, DateTime releaseDate, float profit, string genre, string platform)
-      {
-          this.title = title;
-          this.releaseDate = releaseDate;
-          this.profit = profit;
-          this.genre = genre;
-          this.platform = platform;
-      }
 
-      //Constructor de copiere
-      public Movie(Movie movie)
-      {
-          this.title = movie.title;
-          this.releaseDate = movie.releaseDate;
-          this.profit = movie.profit;
-          this.genre = movie.genre;
-          this.platform = movie.platform;
-      }
+        //Constructor cu parametri
+        public Movie(string title, DateTime releaseDate, float profit, string genre, string platform)
+        {
+            this.title = title;
+            this.releaseDate = releaseDate;
+            this.profit = profit;
+            this.genre = genre;
+            this.platform = platform;
+        }
+
+        //Constructor de copiere
+        public Movie(Movie movie)
+        {
+            this.title = movie.title;
+            this.releaseDate = movie.releaseDate;
+            this.profit = movie.profit;
+            this.genre = movie.genre;
+            this.platform = movie.platform;
+        }
 
         //Proprietati
         public string Title
         {
-            get { 
-                return title; 
+            get {
+                return title;
             }
             set
             {
                 if (value != null)
-                    this.title = value;
+                    this.title = value; //value joaca rol de parametru pe care il primeam la seter
             }
         }
-}
+
+        //Functie de afisare - Varianta 1 - Clasic
+        public void afisare()
+        {
+            Console.WriteLine("Filmul {0}, are profitul {1}, genul {2} si premierea la data de {3} ",
+                title, profit, genre, releaseDate);
+        }
+    }
+
 }
--- 1. Code/S02_IerarhieDeClase/Program.cs
+++ 1. Code/S02_IerarhieDeClase/Program.cs
@@ -10,6 +10,33 @@ namespace S02_IerarhieDeClase
     {
         static void Main(string[] args)
         {
+            //Constructor implicit - GRESIT APELAT
+          //  Movie movie1;
+
+            //Constructor implicit - CORECT APELAT
+            Movie movie1 = new Movie();
+
+            //Constructor cu parametri
+            Movie movie2 = new Movie("Singur acasa", new DateTime(), 12.5f, "Comedy", "NETFLIX");
+
+
+            //Constructor de copiere - GRESIT APELAT
+            //Movie movie3(movie2);
+
+            //Constructor de copiere - GRESIT APELAT - SHALLOW COPY
+           // Movie movie3 = movie2;
+
+            //Constructor de copiere - CORECT APELAT - DEEP COPY
+             Movie movie3 = new Movie(movie2);
+
+            //Proprietate
+            movie2.Title = "Harry Potter";          // se apeleaza seterul din proprietate
+            Console.WriteLine(movie2.Title);        //se apeleaza geterul din proprietate
+
+            //Metoda de afisare - clasic
+            movie1.afisare();
+            movie2.afisare();
+            movie3.afisare();
         }
     }
 }
